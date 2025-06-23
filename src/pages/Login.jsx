c7eac310d521d01518ad4ba6e8b59a6481a11530
Mauro <mauroassiscumbane3@gmail.com>
2025-06-23 12:54:34 -0700

    Nova funcionalidade X

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ export default function Login() {
 
 
         try {
-            const response = await fetch("http://localhost:5432/api/login", {
+            const response = await fetch("https://backendstock-zyx4.onrender.com/api/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
--- src/services/api.js
+++ src/services/api.js
@@ -5,4 +5,4 @@ const api = axios.create({
   baseURL: 'https://backendstock-zyx4.onrender.com/api', // troque se necessário
 });
 
-expo
+export default api;
